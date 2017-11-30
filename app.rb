5dfb5b19dbc41cb819c7923a37ccb425459f6132
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2017-11-30 10:40:58 +0530

    guess feature passed

--- app.rb
+++ app.rb
@@ -39,7 +39,7 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    flash[:message] = "You have already used that letter" if !@game.guess letter
     redirect '/show'
   end
   
